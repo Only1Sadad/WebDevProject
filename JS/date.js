68b4ebe152362d2f59fb3693ba733838b288411b
Sadad Yusuf <ysadad96@gmail.com>
2025-11-27 09:38:00 -0500

    Thu-Nov-27-25

---
+++ JS/date.js
@@ -0,0 +1,4 @@
+//how to get current date
+//document.getElementById("date").textContent = new Date().toLocaleString;
+const now = new Date();
+document.getElementById("todays-date").textContent = now;
